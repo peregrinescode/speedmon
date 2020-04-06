c27a96fb882148f910c947625c97d696413f2d1d
Ross Warren <peregrine.warren@physics.ox.ac.uk>
2020-04-06 15:39:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# speedmon
+
+Cron job which runs speedtest and logs to file.
---
+++ cron_speedtest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+LOG_PATH="/home/ross/code/speedtest.log"
+
+if result=$(/usr/bin/speedtest --simple); then
+  parsed_result=$(printf "${result}\"" | sed ':a;N;$!ba;s/\n/" /g' | sed 's/: /="/g')
+  printf "[$(date)] ${parsed_result}\n" >> "${LOG_PATH}"
+else
+  printf "[$(date)] error\n" >> "${LOG_PATH}"
+  exit 1
+fi
+exit 0
