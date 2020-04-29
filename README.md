a7931d2e1de1d524dba4537afb5b22c729b6ab77
Ross Warren <peregrine.warren@physics.ox.ac.uk>
2020-04-29 13:03:41 +0100

    box plot

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Cron job which runs speedtest and logs to file.
 
-Python script which plots the results.
+Python script which plots the results in a histogram and boxplot:
 
 ![histogram](speed_histogram.png "Example output")
+
+
+![boxplot](speed_v_time.png "Example output")
--- plot_speeds.py
+++ plot_speeds.py
@@ -9,6 +9,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import seaborn as sns
 sns.set_style('darkgrid')
 import datetime
@@ -31,7 +32,7 @@ def read_speedLog(log_in):
 
     # Convert Time column to python datetime object
     for i, row in logf.iterrows():
-        logf['Time'][i]= logf['Time'][i].replace('[', '')
+        logf['Time'][i] = logf['Time'][i].replace('[', '')
         logf['Time'][i]= logf['Time'][i].replace(']', '')
         logf['Time'][i]= logf['Time'][i].replace(' BST', '')
         logf['Time'][i]= logf['Time'][i].replace(' GMT', '')
@@ -61,10 +62,10 @@ def read_speedLog(log_in):
 
 
 def plot_hist(logP):
+    '''Plot histgram from speedtest logfile.'''
 
     fig, ax = plt.subplots(3, 1)
 
-    
     sns.distplot(logP['Ping'], kde=False, hist_kws={"range": [0,1000]}, ax=ax[0])
     sns.distplot(logP['Download'], kde=False, hist_kws={"range": [0,20]}, ax=ax[1])
     sns.distplot(logP['Upload'], kde=False, hist_kws={"range": [0,5]}, ax=ax[2])
@@ -79,9 +80,55 @@ def plot_hist(logP):
     fig.set_size_inches(4, (4 / 1.618) * 3)
     fig.subplots_adjust(hspace=0.4)
     fig.savefig('speed_histogram.png', pad_inches=0.1, bbox_inches='tight', dpi=300)
-    #plt.show()
+
+def plot_time(logP):
+    '''Plot speed against time from speedtest logfile.'''
+
+    fig, ax = plt.subplots(3, 1)
+
+    # Conver datetime to just hours:mins.
+    for i, row in logP.iterrows():
+        # logP['Time'][i] = pd.to_datetime(logP['Time'][i]).strftime('%H:%M')
+        logP['Time'][i] = pd.to_datetime(logP['Time'][i]).strftime('%H')
+        # logP['Time'][i] = mdates.date2num(logP['Time'][i])
+        #hour = logP['Time'][i].hour
+        #mins = logP['Time'][i].minute
+        #y = datetime.time(hour, mins)
+        #x.append(y)
+    #logP.insert(4, "Hours", x)
+
+    x = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22']
+    ping_time = pd.DataFrame(columns=x)
+    up_time = pd.DataFrame(columns=x)
+    down_time = pd.DataFrame(columns=x)
+    for i in x:
+        ping_time[i] = logP.where(logP['Time'] == i)['Ping']
+        down_time[i] = logP.where(logP['Time'] == i)['Download']
+        up_time[i] = logP.where(logP['Time'] == i)['Upload']
+    
+    sns.boxplot(data = ping_time, ax=ax[0])
+    sns.boxplot(data = down_time, ax=ax[1])
+    sns.boxplot(data = up_time, ax=ax[2])
+
+    # xformatter = mdates.DateFormatter('%H')
+    # ax[0].xaxis.set_major_formatter(xformatter)
+    
+    ax[0].set_ylabel(r'Time ($ms$)')
+    ax[1].set_ylabel(r'Download speed ($Mbit/s$)')
+    ax[2].set_ylabel(r'Upload speed ($Mbit/s$)')
+    ax[0].set_xlabel(r'Hour of day')
+    ax[1].set_xlabel(r'Hour of day')
+    ax[2].set_xlabel(r'Hour of day')
+    ax[0].set_ylim(-10,400)
+    ax[1].set_ylim(-0.5,15)
+    ax[2].set_ylim(-0.1,3)
+
+    fig.set_size_inches(4, (4 / 1.618) * 3)
+    fig.subplots_adjust(hspace=0.4)
+    fig.savefig('speed_v_time.png', pad_inches=0.1, bbox_inches='tight', dpi=300)
 
 if __name__ == "__main__":
     logf = read_speedLog('speedtest.log')
-    plot_hist(logf)
+    # plot_hist(logf)
+    plot_time(logf)
 
